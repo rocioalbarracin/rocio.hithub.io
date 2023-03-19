cc7407eefec567e8ab776feb74e498edde161f78
rocioalbarracin <122891650+rocioalbarracin@users.noreply.github.com>
2023-03-19 15:04:57 -0300

    Delete funcionCV.js

--- funcionCV.js
+++
@@ -1,9 +0,0 @@
-
-document.getElementById('mail').addEventListener('click',function(){
-    document.getElementById('mensaje').style.webkitTextFillColor='#FF0000';
-    document.getElementById('enviado').addEventListener('click', function(){
-    });
-});
-
-
-
