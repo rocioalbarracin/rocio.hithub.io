19e88407f2f6b27314a9ae7608824068ede22dd4
rocioalbarracin <122891650+rocioalbarracin@users.noreply.github.com>
2023-03-19 14:50:17 -0300

    Update funcionCV.js

--- funcionCV.js
+++ funcionCV.js
@@ -1,15 +1,18 @@
-
-  
-function mostrarDireccion() {
+<script>
+  function mostrarDireccion() {
     document.getElementById("direccion").innerHTML = "Open Door, Provincia Bs. As.";
 }
+</script>
+<script>
 function mostrarTelefono() {
     document.getElementById("telefono").innerHTML = "2323655516";
 }
+</script>
+<script>
 function mostrarEmail() {
     document.getElementById("E-mail").innerHTML = "rocio_albarracin@yahoo.com.ar";
 }
-
+</script>
 document.getElementById('mail').addEventListener('click',function(){
     document.getElementById('mensaje').style.webkitTextFillColor='#FF0000';
     document.getElementById('enviado').addEventListener('click', function(){
