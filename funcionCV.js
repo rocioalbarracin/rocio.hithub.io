e36e68d050f5976ce7e60e5c319ff2afff7d1a96
rocioalbarracin <122891650+rocioalbarracin@users.noreply.github.com>
2023-03-19 14:46:42 -0300

    Update funcionCV.js

--- funcionCV.js
+++ funcionCV.js
@@ -1,13 +1,13 @@
 
   
 function mostrarDireccion() {
-    document.getElementById("dir").innerHTML = "Open Door, Provincia Bs. As.";
+    document.getElementById("direccion").innerHTML = "Open Door, Provincia Bs. As.";
 }
 function mostrarTelefono() {
-    document.getElementById("tel").innerHTML = "2323655516";
+    document.getElementById("telefono").innerHTML = "2323655516";
 }
 function mostrarEmail() {
-    document.getElementById("mail").innerHTML = "rocio_albarracin@yahoo.com.ar";
+    document.getElementById("E-mail").innerHTML = "rocio_albarracin@yahoo.com.ar";
 }
 
 document.getElementById('mail').addEventListener('click',function(){
